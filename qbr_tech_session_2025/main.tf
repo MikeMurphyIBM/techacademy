1f154b5e9867d9d15839a4fc11076d7d6ca36b29
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-07-12 11:28:06 -0400

    Update main.tf

--- qbr_tech_session_2025/main.tf
+++ qbr_tech_session_2025/main.tf
@@ -38,12 +38,24 @@ resource "ibm_pi_network" "pvs_network_workspace_a" {
 }
 
 # Create an SSH key 
-# resource  "ibm_pi_key" "ssh_key_a" {
-  #pi_key_name          = "murph2"
-  #pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
-  #provider             = ibm.a
-  #pi_ssh_key           = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyUGbL9JPJ2/2GB/E5tk/zkEQa3ZDt7+lBi36A+pMF/iMtVBcCTj7lsXxJ+QhCZp4Y5yl3amkTzdNUYZmt0PQN8eAmQBVAKS7H5pkRqk7DLfRYmpLvnrYHF3jekqunFaspJGXvLhmfextKkzzlnppXU/o97Rwwj9MOfSqmlv07YEsUbBDHWHak4s1Cm7aSpCRiO0z2tnAsyllCwB/Ha9xqrDrocJqYcBZTA7rOgH08p75JgsOTW2gjSxOgACW/3lRxlOcyh4uZL3bcZBLpiwn+DMeYFdwIt0kpKW4GPqAjqc0m2zyArSv2XaUkhkuecNTmvXX5yTuheDkbygpAakB3Pyrb+wW4GdGGAuWxnx6LvuhklDMAZGVpzVt3M7QZIwphFtpkeE40Ia7xN4C4O5lka20IaM2fwT1VyeTgjErDoA8mvBU3fb7cDTrjMUdzH8f+II/ekamg9yvM3NprN4mpADD6cDG0mp6YX74rIJEkdq74DyetyWZ2Cf8XoZjyRis= mikemurphy@Mikes-MBP.attlocal.net"
-}
+ #resource  "ibm_pi_key" "ssh_key_a" {
+ # pi_key_name          = "murph2"
+ # pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
+ # provider             = ibm.a
+ # pi_ssh_key           = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCyUGbL9JPJ2/2GB/E5tk/zkEQa3ZDt7+lBi36A+pMF/iMtVBcCTj7lsXxJ+QhCZp4Y5yl3amkTzdNUYZmt0PQN8eAmQBVAKS7H5pkRqk7DLfRYmpLvnrYHF3jekqunFaspJGXvLhmfextKkzzlnppXU/o97Rwwj9MOfSqmlv07YEsUbBDHWHak4s1Cm7aSpCRiO0z2tnAsyllCwB/Ha9xqrDrocJqYcBZTA7rOgH08p75JgsOTW2gjSxOgACW/3lRxlOcyh4uZL3bcZBLpiwn+DMeYFdwIt0kpKW4GPqAjqc0m2zyArSv2XaUkhkuecNTmvXX5yTuheDkbygpAakB3Pyrb+wW4GdGGAuWxnx6LvuhklDMAZGVpzVt3M7QZIwphFtpkeE40Ia7xN4C4O5lka20IaM2fwT1VyeTgjErDoA8mvBU3fb7cDTrjMUdzH8f+II/ekamg9yvM3NprN4mpADD6cDG0mp6YX74rIJEkdq74DyetyWZ2Cf8XoZjyRis= mikemurphy@Mikes-MBP.attlocal.net"
+#}
+
+# Define your PowerVS Workspace
+resource "ibm_pi_workspace" "pvs_workspace_a" {
+  pi_name              = "murph-qbr-pvs"
+  pi_datacenter        = "wdc06"
+  pi_resource_group_id = data.ibm_resource_group.group.id 
+
+#Referencing existing SSH key
+pi_key_name                = "murph2"
+    pi_cloud_instance_id   = ibm_resource_instance.pvs_workspace_a.guid
+    pi_key_pair_name       = data.ibm_pi_key.murph2_existing_key.name
+
 
 # Create an instance in workspace A
 resource "ibm_pi_instance" "test-instance" {
