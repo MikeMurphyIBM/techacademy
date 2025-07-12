18d50c8c02d586b08555ef62e338f2cc8ad834f2
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-07-12 03:52:21 -0400

    Update main.tf

--- qbr_tech_session_2025/main.tf
+++ qbr_tech_session_2025/main.tf
@@ -41,7 +41,7 @@ resource "ibm_pi_network" "pvs_network_workspace_a" {
 resource  "ibm_pi_key" "ssh_key_a" {
   pi_key_name          = "murph2"
   pi_cloud_instance_id = ibm_resource_instance.pvs_workspace_a.guid
-  pi_ssh_key           = ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxCFkBYYZkECcH0H1yzlu71fh9y26QHNbY7gOUReV5u mikemurphy@Mikes-MBP.attlocal.net
+  pi_ssh_key           = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFxCFkBYYZkECcH0H1yzlu71fh9y26QHNbY7gOUReV5u mikemurphy@Mikes-MBP.attlocal.net"
   provider             = ibm.a
 }
 
