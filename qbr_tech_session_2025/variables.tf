7ef95975259cfad5c846be27b0edd77b0e65e3dd
MikeMurphyIBM <michaelmurphy79@gmail.com>
2025-10-04 11:21:19 -0400

    Update variables.tf

--- qbr_tech_session_2025/variables.tf
+++ qbr_tech_session_2025/variables.tf
@@ -12,7 +12,7 @@ variable "pvs_zone_a" {
 variable "image_id" {
   description = "The ID of the IBM Power Virtual Server image to use for the instance." 
   type        = string # An image ID is typically a string value. 
-  default     = "bd3b3aba-ad44-4201-a29e-70b143837ce4" # **This will set the default image ID to the one you provided.**
+  default     = "2e6159dd-861c-4da8-b267-e3acea71ee59" # **This will set the default image ID to the one you provided.**
 }
 
 
